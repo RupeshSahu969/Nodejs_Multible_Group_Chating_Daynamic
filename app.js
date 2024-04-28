899637a4e52e7e2e71741fdffed78a7c0c28d927
Rupesh Sahu <rupeshsahu969@gmail.com>
2024-04-28 21:01:55 +0530

    ad this new data

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 require("dotenv").config();
+
 const mongoose = require("mongoose");
+
 const express = require("express");
+
 const app = express();
+
 const http = require('http').Server(app);
+
 const io = require('socket.io')(http);
 
 mongoose.connect('mongodb+srv://rupeshsahu969:rupesh@cluster0.iixrcst.mongodb.net/Chhating?retryWrites=true&w=majority&appName=Cluster0')
 
 const userRoute = require("./routes/userRoute");
+
 const User = require('./models/userModel');
+
 const Chat=require('./models/chatModel');
 
 app.use("/", userRoute);
@@ -16,16 +23,21 @@ app.use("/", userRoute);
 const userNamespace = io.of('/user-namespace');
 
 userNamespace.on('connection', async function(socket) {
+
   console.log('User Connected');
 
   var userId = socket.handshake.auth.token;
+
   await User.findByIdAndUpdate({_id: userId}, {$set:{is_online:'1'}});
 
   userNamespace.emit('getOnlineUser', {user_id : userId});
 
   socket.on('disconnect', async function() {
+
       console.log('User Disconnected');
+
       var userId = socket.handshake.auth.token;
+
       await User.findByIdAndUpdate({_id: userId}, {$set:{is_online:'0'}});
       
       userNamespace.emit('getOfflineUser', {user_id : userId});
@@ -33,12 +45,14 @@ userNamespace.on('connection', async function(socket) {
 
   // Handle new chat messages
   socket.on('newChat', function(data) {
+
     socket.broadcast.emit('loadNewChat', data);
   });
 
 // load old chats
 
 socket.on('existsChat',async function(data){
+  
   var chats = await Chat.find({ $or:[
     {sender_id: data.sender_id, receiver_id: data.receiver_id},
 
